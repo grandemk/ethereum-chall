ca297f185070ec99bff1fdfdcf785f14a5ef85bc
Kevin Grandemange <grandemange.kevin@gmail.com>
2017-11-20 23:14:27 +0100

    contract creation automation

---
+++ createContract.sh
@@ -0,0 +1,13 @@
+name=${1%.*}
+
+./compile.sh "$1"
+echo "personal.unlockAccount(eth.coinbase, \"\")"
+echo "${name}Contract = eth.contract(${name}Info.abi)"
+echo "var ${name} = ${name}Contract.new({from:eth.coinbase, data:${name}Info.code, gas:300000})"
+
+echo "miner.start(8)"
+echo "admin.sleepBlocks(4)"
+echo "miner.stop()"
+
+echo "tx = eth.getTransactionReceipt(${name}.transactionHash)"
+echo "${name} = ${name}Contract.at(tx.contractAddress)"
