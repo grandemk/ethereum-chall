e12534d0f3834a8a426a0fcd087f6193be38e85d
Kevin Grandemange <grandemange.kevin@gmail.com>
2017-11-22 23:11:55 +0100

    use a big gas limit to remove not created contract bugs

--- createContract.sh
+++ createContract.sh
@@ -3,7 +3,7 @@ name=${1%.*}
 ./compile.sh "$1"
 echo "personal.unlockAccount(eth.coinbase, \"\")"
 echo "${name}Contract = eth.contract(${name}Info.abi)"
-echo "var ${name} = ${name}Contract.new({from:eth.coinbase, data:${name}Info.code, gas:300000})"
+echo "var ${name} = ${name}Contract.new({from:eth.coinbase, data:${name}Info.code, gas:300000000})"
 
 echo "miner.start(8)"
 echo "admin.sleepBlocks(2)"
